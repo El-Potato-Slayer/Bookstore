25193206b8f724042b46016510791f8a77b80c55
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-02 15:21:53 +0400

    Create containers and components folders, and 2 files

---
+++ src/containers/BooksList.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable semi */
+/* eslint-disable no-unused-vars */
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class BooksList extends Component {
+  render() {
+    return <div>sdf</div>;
+  }
+}
+
+export default connect()(BooksList);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <Provider>
@@ -10,7 +10,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
