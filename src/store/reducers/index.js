e82f9a7ea43a028c6694147791ce282feb1021a4
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-07 00:27:14 +0200

    renamed to filter.js

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
 import { booksReducer } from './booksReducer';
-import filterReducer from './filterReducer';
+import filterReducer from './filter';
 
 export default combineReducers({ booksReducer, filterReducer });
