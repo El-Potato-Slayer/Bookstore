6c9b5f265e8dcdd30c0f477a6080c48ed56cf8eb
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-06 13:04:04 +0200

    added actions to add and remove book

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,15 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { removeBook } from '../store/actions';
 
-class BooksList extends Component {
-  render() {
-    const { books } = this.props;
-    return books.map((singleBook) => (
-      <Book key={singleBook.uniqueId} book={singleBook} />
-    ));
-  }
+function BooksList(props) {
+  const { books } = props;
+  const handleRemoveBook = (book) => {
+    removeBook(book);
+  };
+  return (
+    <table>
+      {
+        books.map((book) => (
+          <tr key={book.id}>
+            <Book key={book.id} book={book} removeBook={handleRemoveBook(book)} />
+            <td>
+              <button type="button" onClick={handleRemoveBook(book)}>Remove</button>
+            </td>
+          </tr>
+        ))
+      }
+    </table>
+  );
 }
 
 const mapStateToProps = (state) => ({
